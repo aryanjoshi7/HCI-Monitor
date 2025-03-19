5aafb7cd97925d4884ed1e9075e5c4e19c7b8d9f
adkolli <aditkk29@gmail.com>
2025-03-19 16:07:48 -0400

    Working on pose detection

--- camera_stuff/camera_demo.py
+++ camera_stuff/camera_demo.py
@@ -1,6 +1,9 @@
-import cv3
+import cv2
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 import numpy as np
 
 
@@ -10,7 +13,7 @@ def draw_landmarks_on_image(rgb_image, detection_result):
 
     # Loop through the detected poses to visualize.
     for idx in range(len(pose_landmarks_list)):
-    pose_landmarks = pose_landmarks_list[idx]
+        pose_landmarks = pose_landmarks_list[idx]
 
     # Draw the pose landmarks.
     pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
@@ -24,19 +27,9 @@ def draw_landmarks_on_image(rgb_image, detection_result):
         solutions.drawing_styles.get_default_pose_landmarks_style())
     return annotated_image
 
-def setup_landmarker():
-    BaseOptions = mp.tasks.BaseOptions
-    PoseLandmarker = mp.tasks.vision.PoseLandmarker
-    PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
-    VisionRunningMode = mp.tasks.vision.RunningMode
-
-    options = PoseLandmarkerOptions(
-        base_options=BaseOptions(model_asset_path=model_path),
-        running_mode=VisionRunningMode.IMAGE)
-    return PoseLandmarker.create_from_options(options)
 
 # Initialize the camera
-cap = cv3.VideoCapture(0)  # 0 usually corresponds to the default camera
+cap = cv2.VideoCapture(0)  # 0 usually corresponds to the default camera
 
 # Check if the camera opened successfully
 if not cap.isOpened():
@@ -54,17 +47,30 @@ while(True):
         break
 
     # Posture detection
-    image_rgb = cv3.cvtColor(image, cv3.COLOR_BGR2RGB)
-    landmarker = setup_landmarker()
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
-    pose_landmarker_result = landmarker.detect(mp_image)
+    cv2.imshow('frame', frame)
+    if frame is not None:
+        BaseOptions = mp.tasks.BaseOptions
+        PoseLandmarker = mp.tasks.vision.PoseLandmarker
+        PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+        VisionRunningMode = mp.tasks.vision.RunningMode
+        options = PoseLandmarkerOptions(
+            base_options=BaseOptions(model_asset_path='/Users/aditkolli/Desktop/HCI-Monitor/HCI-Monitor/pose_landmarker_full.task'),
+            running_mode=VisionRunningMode.IMAGE)
 
+        with PoseLandmarker.create_from_options(options) as landmarker:
+            print(frame)
+            image_rgb = np.array(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image_rgb)
+            pose_landmarker_result = landmarker.detect(mp_image)
+            print(pose_landmarker_result)
+            annotated_image = draw_landmarks_on_image(mp_image.numpy_view(), pose_landmarker_result)
+            cv2.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
 
 
     # Exit on pressing 'q'
-    if cv3.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # Release the camera and close windows
 cap.release()
-cv3.destroyAllWindows()
+cv2.destroyAllWindows()
